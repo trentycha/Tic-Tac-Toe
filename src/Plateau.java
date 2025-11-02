06fbc80e76bca31be7b62581c8c38d1dd76abbb9
trentycha <trentycharlene@gmail.com>
2025-11-02 14:38:12 +0100

    Ajout du plateau de jeu

---
+++ src/Plateau.java
@@ -0,0 +1,59 @@
+public class Plateau {
+
+    private Case[][] plateauJeu;
+
+    public Plateau() {
+        plateauJeu = new Case[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                plateauJeu[i][j] = new Case();
+            }
+        }
+    }
+
+    public boolean placerSymbole(int ligne, int colonne, char symbole) {
+        if(plateauJeu[ligne][colonne].estVide()) {
+            plateauJeu[ligne][colonne].setSymbole(symbole);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean victoire() {
+        for(i = 0; i < 3; i++) {
+            if(plateauJeu[i][0].getSymbole() == symbole && plateauJeu[i][1].getSymbole() == symbole && plateauJeu[i][2].getSymbole() == symbole) {
+                return true;
+            }
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (plateauJeu[0][j].getSymbole() == symbole && plateauJeu[1][j].getSymbole() == symbole && plateauJeu[2][j].getSymbole() == symbole) {
+                return true;
+            }
+        }
+
+        if (plateauJeu[0][0].getSymbole() == symbole && plateauJeu[1][1].getSymbole() == symbole && plateauJeu[2][2].getSymbole() == symbole) {
+            return true;
+        }
+
+        if (plateauJeu[0][2].getSymbole() == symbole && plateauJeu[1][1].getSymbole() == symbole && plateauJeu[2][0].getSymbole() == symbole) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean estPlein() {
+        for(i = 0; i < 3; i++) {
+            for(j = 0; j < 3; j++) {
+                if(plateauJeu[i][j].estVide()) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    }
+
+}
